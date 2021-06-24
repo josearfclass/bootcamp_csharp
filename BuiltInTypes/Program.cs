e426d8efcb34c606c28d5eec9c8a2b594d6eeefd
jose <josearfclass@gmail.com>
2021-06-23 23:00:13 -0400

    BuiltInTypes, struct, value types

---
+++ BuiltInTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuiltInTypes
+{
+    class Program
+    {
+        public struct DetailedInteger
+        {
+            public int Number;
+            private List<string> detail;
+
+            public DetailedInteger(int Number)
+            {
+                this.Number = Number;
+                detail = new List<string>();
+            }
+
+            public void AddDetail(string detail)
+            {
+                this.detail.Add(detail);
+            }
+
+            public override string ToString()
+            {
+                return $"{this.Number} [{string.Join(", ", detail)}]";
+            }
+        }
+        public static void Main()
+        {
+            var n1 = new DetailedInteger(0);
+            n1.AddDetail("A");
+            Console.WriteLine(n1);
+
+            var n2 = n1;
+            n2.Number = 7;
+            n2.AddDetail("B");
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+    }
+}
