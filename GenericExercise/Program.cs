eafbd286a312226a6c361f369a7734e0d424d1ea
jose <josearfclass@gmail.com>
2021-06-28 15:31:00 -0400

    Exercise for generic class methods

---
+++ GenericExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GenericExercise
+{
+    class Program
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T c = a;
+            a = b;
+            b = c;
+        }
+
+        public static void Main()
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+
+            Console.WriteLine("Before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            Console.WriteLine("After calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+        }
+    }
+}
