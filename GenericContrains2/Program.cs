7cdf8e88bc9e91fdc8bb75a4089eccdb372d013d
jose <josearfclass@gmail.com>
2021-06-28 23:13:19 -0400

    GenericConstraint new(), create like singleton

---
+++ GenericContrains2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GenericContrains2
+{
+    class Program
+    {
+        public class UniqueInstance<T> where T : new()
+        {
+            public static T Unique = new();
+        }
+
+        class Person : UniqueInstance<Person>
+        {
+            public string Name;
+        }
+
+        static void Main(string[] args)
+        {
+            Person.Unique.Name = "pepe";
+            Console.WriteLine(Person.Unique.Name);
+        }
+    }
+}
